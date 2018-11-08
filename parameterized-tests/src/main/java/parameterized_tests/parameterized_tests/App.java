b30049a99a7d020dc52c3fb0ce156d48c9128fae
Matteo Orrù <matt.orru@gmail.com>
2018-11-08 10:31:09 +0100

    sync

---
+++ parameterized-tests/src/main/java/parameterized_tests/parameterized_tests/App.java
@@ -0,0 +1,13 @@
+package parameterized_tests.parameterized_tests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
